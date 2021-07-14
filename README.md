91088017b5e8e51499abc813455d8f2bb67a0794
wedsleyfernando <wedsleyfernando@gmail.com>
2021-07-14 18:51:52 -0300

    Add Readme.md, and make it install with one line

---
+++ README.md
@@ -0,0 +1,16 @@
+# TMUX Config Install
+
+## How to Install
+---
+Install this config with one Line 
+
+    bash <(curl -s https://raw.githubusercontent.com/wmilou/tmux/master/install.sh)
+
+--- 
+
+## Active it
+
+---
+To install theme and plugins 
+> inside tmux type [prefix] + [i] 
+---
--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash 
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp tmux.conf ~/.tmux.conf
+# Colors definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
+install_tpm_plugin(){
+    printf "${GREEN}Instaling TPM plugin manager config ... ${NC} \n"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    printf "${GREEN}${BOLD}Tpm Instaled ! ${NC} \n"
+}
+
+install_tmux_configs(){
+    printf "${GREEN}Instaling TMUX config ... ${NC} \n"
+    mv ~/.tmux.conf ~/.tmux.conf_$(date +%F_%R)
+    git clone https://github.com/wmilou/tmux /tmp/tmux_config.conf
+    cp /tmp/tmux_config.conf ~/.tmux.conf
+    printf "${GREEN}${BOLD}Config Instaled ! ${NC} \n"
+}
+
+install_tpm_plugin
+install_tmux_configs
